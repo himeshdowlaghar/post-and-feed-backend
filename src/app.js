91c97728b511fa955ea5ff346635a076f9173850
himeshdowlaghar <himeshdowlaghar@gmail.com>
2026-07-01 14:57:05 +0530

    Add Delete Endpoint for posts

--- src/app.js
+++ src/app.js
@@ -35,7 +35,12 @@ app.post("/upload", upload.single("image"), async (req,res)=>{
 
 
 app.delete("/delete/:id",async(req,res)=>{
-     
+    const id = req.params.id;
+    const data = await postModel.findByIdAndDelete(id);
+    res.status(200).json({
+        data : data,
+        Message : "This data has been deleted Successfuly"
+    })
 })
 
 module.exports = app;
